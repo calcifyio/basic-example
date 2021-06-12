b43374b6d8c426039a60999e2b5c68718c025afe
Hayden Pennington <haydenpennington@icloud.com>
2021-06-12 14:31:44 -0500

    Add shit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask
 import subprocess
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_calcfiy():
     output = subprocess.check_output(['nvidia-smi'], shell=True).decode()
