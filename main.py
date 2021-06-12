b2625851aefda61ae636d3d15b7810a7685217c4
Hayden Pennington <haydenpennington@icloud.com>
2021-06-12 15:42:07 -0500

    v19

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello_calcfiy():
     output = subprocess.check_output(['nvidia-smi'], shell=True).decode()
     lines = output.split('\n')
-    html = 'Hello, Calcify v18</br>'
+    html = 'Hello, Calcify v19</br>'
     for line in lines:
         html += '<div>' + line + '</div>'
     return html
