733422a824ee111f72f6fd6c5cdbfc002c482888
Hayden Pennington <haydenpennington@icloud.com>
2021-06-13 17:54:37 -0500

    use numpy

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from flask import Flask
+import numpy as np
 import subprocess
 app = Flask(__name__)
 
 @app.route('/')
 def hello_calcfiy():
+    n = np.random(10)
     output = subprocess.check_output(['nvidia-smi'], shell=True).decode()
     lines = output.split('\n')
-    html = 'Hello, Calcify New</br>'
+    html = 'Hello, Calcify New ' + str(n) + '</br>'
     for line in lines:
         html += '<div>' + line + '</div>'
     return html
