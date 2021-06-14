f7c0b2d3dbca4bf5f6152f75866a5407e34fa267
Hayden Pennington <haydenpennington@icloud.com>
2021-06-13 19:27:55 -0500

    Mention torch.cuda

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello_calcfiy():
     output = subprocess.check_output(['nvidia-smi'], shell=True).decode()
     lines = output.split('\n')
-    html = 'CUDA: ' + str(torch.cuda.is_available()) + '</br>'
+    html = 'torch.cuda.is_available(): ' + str(torch.cuda.is_available()) + '</br>'
     for line in lines:
         html += '<div>' + line + '</div>'
     return html
