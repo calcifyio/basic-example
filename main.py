eafedfe2f17d842784d5a75c82faa34e26c2c5c5
Hayden Pennington <haydenpennington@icloud.com>
2021-06-13 19:24:51 -0500

    Remove numpy

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from flask import Flask
-import numpy as np
 import torch
 import subprocess
 app = Flask(__name__)
 
 @app.route('/')
 def hello_calcfiy():
-    n = np.random.randn()
     output = subprocess.check_output(['nvidia-smi'], shell=True).decode()
     lines = output.split('\n')
     html = 'CUDA: ' + str(torch.cuda.is_available()) + '</br>'
